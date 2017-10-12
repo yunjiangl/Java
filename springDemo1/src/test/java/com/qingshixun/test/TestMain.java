94edc75330ea26b652cb3bcaf7ce1a752ce2579d
芸江 <1205445409@qq.com>
2017-10-12 14:16:37 +0800

    修改

--- springDemo1/src/test/java/com/qingshixun/test/TestMain.java
+++ springDemo1/src/test/java/com/qingshixun/test/TestMain.java
@@ -7,18 +7,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.spring.beans.Category;
 import com.spring.beans.Product;
 import com.spring.service.ProductService;
+import com.spring.service.Vehicle;
 
 public class TestMain {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[] { "applicationContext.xml" });
 		
-		Category category = (Category) context.getBean("c");
+//		Category category = (Category) context.getBean("c");
+//		
+//		ProductService product = (ProductService) context.getBean("s");
+//		
+//		System.out.println(category.getName());
+//		
+//		product.doSomeService();
 		
-		ProductService product = (ProductService) context.getBean("s");
-		
-		System.out.println(category.getName());
-		
-		product.doSomeService();
+		Vehicle vehicle = (Vehicle) context.getBean("vehicle");
+		vehicle.go();
 	}
 }
